e61c5b80754915585b6d3ca2528426e5557a6f76
XIII___ <cyjallet@gmail.com>
2023-03-08 17:27:49 +0100

    Ajout d'un BukkitListener

---
+++ src/main/java/com/xiii/libertycity/core/processors/bukkit/BukkitListener.java
@@ -0,0 +1,14 @@
+package com.xiii.libertycity.core.processors.bukkit;
+
+import com.xiii.libertycity.LibertyCity;
+import org.bukkit.event.Listener;
+
+public class BukkitListener implements Listener {
+
+    private final LibertyCity plugin;
+
+    public BukkitListener(LibertyCity plugin) {
+        this.plugin = plugin;
+    }
+
+}
