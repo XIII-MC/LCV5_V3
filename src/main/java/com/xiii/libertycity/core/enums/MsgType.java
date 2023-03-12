c8f456697d15c4eeddf42502365164791bfc28d3
XIII___ <cyjallet@gmail.com>
2023-03-12 21:17:36 +0100

    ajout pour le nouveau systeme d'enregistrement

--- src/main/java/com/xiii/libertycity/core/enums/MsgType.java
+++ src/main/java/com/xiii/libertycity/core/enums/MsgType.java
@@ -1,7 +1,12 @@
 package com.xiii.libertycity.core.enums;
 
 public enum MsgType {
-    PREFIX("§2§lLiberty§a§lCity §7» ");
+    PREFIX("§2§lLiberty§a§lCity §7»§f "),
+    MAIRIE("§6§lMairie §7»§f "),
+    MICHEL_FULL("§e§l[NPC]§r§e Michel: §r"),
+    STEVE_FIRST("§e§l[NPC]§r§e Steve §kRowland§r§e: §r"),
+    STEVE_FULL("§e§l[NPC]§r§e Steve Rowland: §r"),
+    STEVE_HIDDEN("§e§l[NPC]§r§e §kSteve Rowland§r§e: §r");
 
     private final String message;
 
