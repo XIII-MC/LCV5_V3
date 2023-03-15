34774b6391e0704099466f1bcdf495f6256839a0
XIII___ <cyjallet@gmail.com>
2023-03-15 21:39:38 +0100

    TimeUtils

---
+++ src/main/java/com/xiii/libertycity/core/utils/time/TimeFormat.java
@@ -0,0 +1,5 @@
+package com.xiii.libertycity.core.utils.time;
+
+public enum TimeFormat {
+    BASIC, HOUR, FULL, LOG, LOG_DATE
+}
---
+++ src/main/java/com/xiii/libertycity/core/utils/time/TimeUtils.java
@@ -0,0 +1,31 @@
+package com.xiii.libertycity.core.utils.time;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtils {
+
+    private static final DateFormat basicFormat = new SimpleDateFormat("dd/MM/yyyy");
+    private static final DateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final DateFormat fullFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    private static final DateFormat logFormat = new SimpleDateFormat("HH:mm:ss:SSSS dd/MM/yyyy");
+    private static final DateFormat logDateFormat = new SimpleDateFormat("dd_MM_yyy");
+
+    public static String convertMillis(final long millis, TimeFormat formatType) {
+
+        final Date date = getDate(millis);
+
+        if (formatType == TimeFormat.BASIC) return basicFormat.format(date);
+        if (formatType == TimeFormat.HOUR) return hourFormat.format(date);
+        if (formatType == TimeFormat.FULL) return fullFormat.format(date);
+        if (formatType == TimeFormat.LOG) return logFormat.format(date);
+        if (formatType == TimeFormat.LOG_DATE) return logDateFormat.format(date);
+
+        return null;
+    }
+
+    public static Date getDate(final long millis) {
+        return new Date(millis);
+    }
+}
