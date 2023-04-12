27ebc2c66df5144ee213058c7285e7a8a0464c94
XIII___ <cyjallet@gmail.com>
2023-04-12 14:17:18 +0200

    Improved some InventoryUtils

--- src/main/java/com/xiii/libertycity/core/utils/InventoryUtils.java
+++ src/main/java/com/xiii/libertycity/core/utils/InventoryUtils.java
@@ -11,48 +11,48 @@ import java.util.Arrays;
 public final class InventoryUtils {
 
     public static ItemStack createItem(final Material material, final int amount, final String name, final String... lore) {
-
         final ItemStack item = new ItemStack(material, amount);
-
         final ItemMeta meta = item.getItemMeta();
-
         meta.setDisplayName(name);
         meta.setLore(Arrays.asList(lore));
         item.setItemMeta(meta);
-
         return item;
     }
 
-    public static void removeOne(final Inventory inventory, final ItemStack itemStack) {
-        final ItemStack one = itemStack.clone();
+    public static void removeOne(final Inventory inventory, final ItemStack itemStack, final boolean skipMetaData) {
+        ItemStack one = itemStack.clone();
+        if (skipMetaData) one = new ItemStack(itemStack.getType());
         one.setAmount(1);
         inventory.removeItem(one);
     }
 
-    public static int getAmount(final Player player, final ItemStack itemStack) {
+    public static int getAmount(final Inventory inv, final ItemStack itemStack, final boolean notNullable, final boolean skipMetaData) {
 
         if (itemStack == null) return 0;
 
         int amount = 0;
         for (int i = 0; i < 36; i++) {
 
-            final ItemStack slot = player.getInventory().getItem(i);
+            final ItemStack slot = inv.getItem(i);
 
-            if (slot == null || !slot.isSimilar(itemStack)) continue;
+            if (!skipMetaData && (slot == null || !slot.isSimilar(itemStack))) continue;
+            if (skipMetaData && (slot == null || !slot.getType().equals(itemStack.getType()))) continue;
 
             amount += slot.getAmount();
         }
 
-        return amount;
+        return notNullable ? amount == 0 ? 1 : amount : amount;
     }
 
-    public static boolean hasItem(final Player player, final ItemStack itemStack) {
+    public static boolean hasItem(final Player player, final ItemStack itemStack, final boolean skipMetaData) {
 
         for (final ItemStack item : player.getInventory().getContents()) {
 
             if (item == null) continue;
 
-            if (item.isSimilar(itemStack)) return true;
+            if (skipMetaData) {
+                if (item.getType().equals(itemStack.getType())) return true;
+            } else if (item.isSimilar(itemStack)) return true;
         }
 
         return false;
