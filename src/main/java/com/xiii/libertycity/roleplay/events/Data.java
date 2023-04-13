4fb89449833bfc34bef3c38daa2cde1ac32573d1
XIII___ <cyjallet@gmail.com>
2023-04-13 17:25:04 +0200

    made those baddies final

--- src/main/java/com/xiii/libertycity/roleplay/events/Data.java
+++ src/main/java/com/xiii/libertycity/roleplay/events/Data.java
@@ -1,10 +1,10 @@
 package com.xiii.libertycity.roleplay.events;
 
-import com.xiii.libertycity.core.processors.ClientPlayPacket;
-import com.xiii.libertycity.core.processors.ServerPlayPacket;
+import com.xiii.libertycity.core.processors.network.packet.ClientPlayPacket;
+import com.xiii.libertycity.core.processors.network.packet.ServerPlayPacket;
 
 public interface Data {
-    void handle(ClientPlayPacket packet);
+    void handle(final ClientPlayPacket packet);
 
-    void handle(ServerPlayPacket packet);
+    void handle(final ServerPlayPacket packet);
 }
