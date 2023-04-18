f6663701592a906e051112fedfce3303eae1794d
XIII___ <cyjallet@gmail.com>
2023-04-18 12:04:22 +0200

    Trying to create empty tab list

---
+++ src/main/java/com/xiii/libertycity/core/utils/TabListUtils.java
@@ -0,0 +1,18 @@
+package com.xiii.libertycity.core.utils;
+
+import com.google.common.collect.Table;
+import com.keenant.tabbed.item.BlankTabItem;
+import com.keenant.tabbed.item.PlayerTabItem;
+import com.keenant.tabbed.tablist.SimpleTabList;
+import com.keenant.tabbed.tablist.TableTabList;
+import com.keenant.tabbed.tablist.TitledTabList;
+import com.xiii.libertycity.LibertyCity;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public final class TabListUtils {
+
+    public static void emptyTabList(final Player player) {
+        LibertyCity.getInstance().getTabInstance().newTableTabList(player, 1, 1, 1);
+    }
+}
