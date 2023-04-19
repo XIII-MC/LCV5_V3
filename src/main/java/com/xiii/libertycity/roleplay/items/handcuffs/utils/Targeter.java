5434b66d0a1cd1f5223212cd100935a0a8dbc075
XIII___ <cyjallet@gmail.com>
2023-04-19 13:26:29 +0200

    a

--- src/main/java/com/xiii/libertycity/roleplay/items/handcuffs/utils/Targeter.java
+++ src/main/java/com/xiii/libertycity/roleplay/items/handcuffs/utils/Targeter.java
@@ -1,5 +1,6 @@
 package com.xiii.libertycity.roleplay.items.handcuffs.utils;
 
+import com.xiii.libertycity.core.utils.ChatUtils;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
@@ -14,6 +15,10 @@ public final class Targeter {
         return getTarget(entity, entity.getWorld().getEntities());
     }
 
+    public static boolean getTargetHitBox(final Entity entity) {
+        return getHitBox(entity, entity.getWorld().getEntities()) != null;
+    }
+
     public static <T extends Entity> T getTarget(final Entity entity,
                                                  final Iterable<T> entities) {
         if (entity == null)
@@ -23,6 +28,8 @@ public final class Targeter {
         for (final T other : entities) {
             final Vector n = other.getLocation().toVector()
                     .subtract(entity.getLocation().toVector());
+            ChatUtils.broadcast("" + entity.getLocation().getDirection().normalize().crossProduct(n)
+                    .lengthSquared());
             if (entity.getLocation().getDirection().normalize().crossProduct(n)
                     .lengthSquared() < threshold
                     && n.normalize().dot(
@@ -34,6 +41,26 @@ public final class Targeter {
                     target = other;
             }
         }
+        return target;
+    }
+
+    public static <T extends Entity> T getHitBox(final Entity entity,
+                                                 final Iterable<T> entities) {
+        if (entity == null) return null;
+        T target = null;
+        int count = 0;
+        for (final T other : entities) {
+            final Vector n = other.getLocation().toVector()
+                    .subtract(entity.getLocation().toVector());
+            final double result = entity.getLocation().getDirection().normalize().crossProduct(n).lengthSquared();
+            if (result < 1000 && result > 1) {
+                count++;
+                ChatUtils.broadcast("" + result);
+                if (count >= 10) return null;
+                else target = other;
+            } else target = other;
+        }
+
         return target;
     }
 
